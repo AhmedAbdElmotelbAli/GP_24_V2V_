2af6ddc1cffea20afc01f653a2c548c194aa636f
Karim Aql <101567931+KarimAql@users.noreply.github.com>
2024-01-29 17:39:18 +0200

    Updated UltraSonic.c

--- UltraSonic/UltraSonic.c
+++ UltraSonic/UltraSonic.c
@@ -1,8 +1,3 @@
-/* This example code Measures the distance using HC-SR04 Ultrasonic range sensor*/
-/* It displays the measured distance value on computer using UART communication moduel of TM4C123 */
-/* Timer0A is used to measure distance by measuring pulse duration of Echo output signal */
-/* Timer1A is used to make percise microsecond delay function */
-
 /*header files for TM4C123 device and sprintf library */
 #include "TM4C123GH6PM.h"
 #include <stdio.h>
