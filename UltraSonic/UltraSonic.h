fdf196470a5f8490a16cf2c8fd1e97a227a9a0b2
Karim Aql <101567931+KarimAql@users.noreply.github.com>
2024-02-09 07:02:56 +0200

    Update UltraSonic.h

--- UltraSonic/UltraSonic.h
+++ UltraSonic/UltraSonic.h
@@ -1,12 +1,10 @@
-#ifndef US_H_
-#define US_H_
+#ifndef ULTRASONIC_H_
+#define ULTRASONIC_H_
 
-uint32_t Measure_distance(void);
+void measure_distance(void);
+uint32_t measure_pulse_duration(void)
 void Timer0ACapture_init(void);
 void Delay_MicroSecond(int time);
-void UART5_init(void);
-void UART5_Transmitter(unsigned char data);
-void printstring(char *str);
 void Delay(unsigned long counter);
 
 #endif
