8cf827f09dda1b93cbd2417ac932267012c61334
Karim Aql <101567931+KarimAql@users.noreply.github.com>
2024-02-09 07:01:37 +0200

    Update UltraSonic.c

--- UltraSonic/UltraSonic.c
+++ UltraSonic/UltraSonic.c
@@ -1,43 +1,34 @@
-/*header files for TM4C123 device and sprintf library */
-#include "TM4C123GH6PM.h"
+#include "tm4c123gh6pm.h"
+#include "UART.h"
 #include <stdio.h>
 
-/*Function prototype for Timer0A and UART module initialization */
-
-uint32_t Measure_distance(void);
+/*functions prototypes*/
+void measure_distance(void);
+uint32_t measure_pulse_duration(void)
 void Timer0ACapture_init(void);
 void Delay_MicroSecond(int time);
-void UART5_init(void);
-void UART5_Transmitter(unsigned char data);
-void printstring(char *str);
 void Delay(unsigned long counter);
 
 /* global variables to store and display distance in cm */
 uint32_t time; /*stores pulse on time */
 uint32_t distance; /* stores measured distance value */
-char mesg[20];  /* string format of distance value */
+int i;  /*iterartor*/
 
 /* main code to take distance measurement and send data to UART terminal */
-int main(void)
+void measure_distance(void)
 {
-Timer0ACapture_init();  /*initialize Timer0A in edge edge time */
-UART5_init(); /* initialize UART5 module to transmit data to computer */
-	while(1)
-	{
-time = Measure_distance(); /* take pulse duration measurement */ 
-distance = (time * 10625)/10000000; /* convert pulse duration into distance */
-sprintf(mesg, "\r\nDistance = %d cm", distance); /*convert float type distance data into string */
-printstring(mesg); /*transmit data to computer */
-Delay(2000);
-
-	}
-	
+    Timer0ACapture_init();  /*initialize Timer0A in edge edge time */
+    UART_INIT(5); /* initialize UART5 module to transmit data to computer */
+    time = Measure_pulse_duration(); /* take pulse duration measurement */ 
+    distance = (time * 10625)/10000000; /* convert pulse duration into distance */
+    for(i = 0; i< 4; i++)
+        UART_TX((distance>>(8*i) & 0XFF), 5); 
 }
 
 
 /* This function captures consecutive rising and falling edges of a periodic signal */
 /* from Timer Block 0 Timer A and returns the time difference (the period of the signal). */
-uint32_t Measure_distance(void)
+uint32_t measure_pulse_duration(void)
 {
     int lastEdge, thisEdge;
 	
@@ -110,37 +101,7 @@ void Delay_MicroSecond(int time)
         TIMER1->ICR = 0x1;      /* clear the TimerA timeout flag */
     }
 }
-void UART5_init(void)
-{
-	  SYSCTL->RCGCUART |= 0x20;  /* enable clock to UART5 */
-    SYSCTL->RCGCGPIO |= 0x10;  /* enable clock to PORTE for PE4/Rx and RE5/Tx */
-    /* UART0 initialization */
-    UART5->CTL = 0;         /* UART5 module disbable */
-    UART5->IBRD = 104;      /* for 9600 baud rate, integer = 104 */
-    UART5->FBRD = 11;       /* for 9600 baud rate, fractional = 11*/
-    UART5->CC = 0;          /*select system clock*/
-    UART5->LCRH = 0x60;     /* data lenght 8-bit, not parity bit, no FIFO */
-    UART5->CTL = 0x301;     /* Enable UART5 module, Rx and Tx */
 
-    /* UART5 TX5 and RX5 use PE4 and PE5. Configure them digital and enable alternate function */
-    GPIOE->DEN = 0x30;      /* set PE4 and PE5 as digital */
-    GPIOE->AFSEL = 0x30;    /* Use PE4,PE5 alternate function */
-    GPIOE->AMSEL = 0;    /* Turn off analg function*/
-    GPIOE->PCTL = 0x00110000;     /* configure PE4 and PE5 for UART */
-}
-void UART5_Transmitter(unsigned char data)  
-{
-    while((UART5->FR & (1<<5)) != 0); /* wait until Tx buffer not full */
-    UART5->DR = data;                  /* before giving it another byte */
-}
-
-void printstring(char *str)
-{
-  while(*str)
-	{
-		UART5_Transmitter(*(str++));
-	}
-}
 void Delay(unsigned long counter)
 {
 	unsigned long i = 0;
