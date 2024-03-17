110a35a6b5e5a0b5d37c952b254d214cdaf02061
Karim Aql <aqlkarim@gmail.com>
2024-03-17 21:36:01 +0200

    updated rc car control
    added PWM speed control

--- Raspberry Pi/rc_car_move.py
+++ Raspberry Pi/rc_car_move.py
@@ -1,44 +1,75 @@
-import RPi.GPIO as gpio
+import RPi.GPIO as GPIO
 import time
-def init():    
-    gpio.setmode(gpio.BCM)
-    gpio.setup(17, gpio.OUT)
-    gpio.setup(22, gpio.OUT)
-    gpio.setup(23, gpio.OUT)
-    gpio.setup(24, gpio.OUT)
-def forward(sec):
-    init()
-    gpio.output(17, True)
-    gpio.output(22, False)
-    gpio.output(23, True)
-    gpio.output(24, False)
-    print("Going Forward..")
-    time.sleep(sec)
-    gpio.cleanup() 
-def backward(sec):
-    init()
-    gpio.output(17, False)
-    gpio.output(22, True)
-    gpio.output(23, False)
-    gpio.output(24, True)
-    print("Going Backward..")
-    time.sleep(sec)
-    gpio.cleanup()
-def left_turn(sec):
-    init()
-    gpio.output(17, False)
-    gpio.output(22, True)
-    gpio.output(23, True)
-    gpio.output(24, False)
-    print("Turning Left..")
-    time.sleep(sec)
-    gpio.cleanup()
-def right_turn(sec):
-    init()
-    gpio.output(17, True)
-    gpio.output(22, False)
-    gpio.output(23, False)
-    gpio.output(24, True)
-    print("Turning Right..")
-    time.sleep(sec)
-    gpio.cleanup()
+
+def init():
+    # Set GPIO mode
+    GPIO.setmode(GPIO.BCM)
+    # Setup GPIO pins
+    GPIO.setup(12, GPIO.OUT)
+    GPIO.setup(17, GPIO.OUT)
+    GPIO.setup(22, GPIO.OUT)
+    GPIO.setup(13, GPIO.OUT)
+    GPIO.setup(23, GPIO.OUT)
+    GPIO.setup(24, GPIO.OUT)
+    # Setup PWM pins
+    pwmA = GPIO.PWM(12, 1000)  # PWM frequency set to 1000Hz
+    pwmB = GPIO.PWM(13, 1000)
+    # Start PWM with 0% duty cycle
+    pwmA.start(0)
+    pwmB.start(0)
+
+    return pwmA, pwmB
+
+def set_speed(pwmA, pwmB, speedA, speedB):
+    pwmA.ChangeDutyCycle(speedA)  # Change speed of motor A
+    pwmB.ChangeDutyCycle(speedB)  # Change speed of motor B
+    print(f"Speed is set to {speedA} %")
+
+def forward(seconds, speed):
+    pwmA, pwmB = init()
+
+    GPIO.output(17, True)
+    GPIO.output(22, False)
+    GPIO.output(23, True)
+    GPIO.output(24, False)
+    set_speed(pwmA, pwmB, speed, speed)
+    print ("Going forward...")
+    time.sleep(seconds)
+    set_speed(pwmA, pwmB, 0, 0)
+    GPIO.cleanup()
+
+def backward(seconds, speed):
+    pwmA, pwmB = init()
+    GPIO.output(17, False)
+    GPIO.output(22, True)
+    GPIO.output(23, False)
+    GPIO.output(24, True)
+    set_speed(pwmA, pwmB, speed, speed)
+    print ("Going backward...")
+    time.sleep(seconds)
+    set_speed(pwmA, pwmB, 0, 0)
+    GPIO.cleanup()
+
+def turn_right(seconds, speed):
+    pwmA, pwmB = init()
+    GPIO.output(17, True)
+    GPIO.output(22, False)
+    GPIO.output(23, False)
+    GPIO.output(24, True)
+    set_speed(pwmA, pwmB, speed, speed)
+    print ("Turning right...")
+    time.sleep(seconds)
+    set_speed(pwmA, pwmB, 0, 0)
+    GPIO.cleanup()
+
+def turn_left(seconds, speed):
+    pwmA, pwmB = init()
+    GPIO.output(17, False)
+    GPIO.output(22, True)
+    GPIO.output(23, True)
+    GPIO.output(24, False)
+    set_speed(pwmA, pwmB, speed, speed)
+    print ("Turning left...")
+    time.sleep(seconds)
+    set_speed(pwmA, pwmB, 0, 0)
+    GPIO.cleanup()
