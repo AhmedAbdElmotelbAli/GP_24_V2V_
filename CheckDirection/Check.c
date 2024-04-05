c29097e3011dc809252ff304357b2d661d1b01b8
AhmedAbdElmotelbAli <104862736+AhmedAbdElmotelbAli@users.noreply.github.com>
2024-04-05 02:14:40 +0200

    Add files via upload

---
+++ CheckDirection/Check.c
@@ -0,0 +1,49 @@
+#include "RTE_Main.h"
+#include "stdbool.h"
+#include "check.h"
+	
+	char validCommands[] = {'B', 'L', 'R', 'G', 'I', 'H', 'J'};
+
+
+bool checksaveDirection(void){
+  bool Save = true;
+
+	if(RTE_Call_Ultrasonic_U32MeasureDistance()<=20&&RTE_Call_Ultrasonic_U32MeasureDistance()>15){
+			Save=false;
+			RTE_Call_LCD_VOIDCleanscreen();
+			RTE_Call_LCD_VoidDisplayStringRowColumn(0,0,"waring speed ");
+			RTE_Call_LCD_VoidDisplayStringRowColumn(1,0,"please low speed ");
+		}else if(RTE_Call_Ultrasonic_U32MeasureDistance()<=15&&RTE_Call_Ultrasonic_U32MeasureDistance()>8){
+			Save=false;
+			RTE_Call_LCD_VOIDCleanscreen();
+			RTE_Call_LCD_VoidDisplayStringRowColumn(0,0,"waring body ");
+			RTE_Call_LCD_VoidDisplayStringRowColumn(1,0,"change direction");
+	}else if(RTE_Call_Ultrasonic_U32MeasureDistance()<=8&&RTE_Call_Ultrasonic_U32MeasureDistance()>0){
+			Save=false;
+			RTE_Call_LCD_VOIDCleanscreen();
+			RTE_Call_LCD_VoidDisplayStringRowColumn(0,0,"waring Accient ");
+			RTE_Call_LCD_VoidDisplayStringRowColumn(1,0,"please stop Car");
+			Motor_VoidStop();
+}
+	return Save;
+}
+
+bool changeDirection(char command) {
+        bool validCommand = false;
+        for (int i = 0; i < (int)(sizeof(validCommands) / sizeof(validCommands[0])); i++) {
+            if (command == validCommands[i]) {
+                validCommand = true;
+                break;
+            }
+        }
+				return validCommand;		
+}
+void delay_s(void){
+	for(int i=0;i<16000000;i++);
+}
+void Delay_s(int x){
+for(int i=0;i<x;i++){
+	delay_s();
+}
+}
+
---
+++ CheckDirection/check.h
@@ -0,0 +1,9 @@
+#ifndef _Check_H_
+#define _Check_H_
+#include "stdbool.h"
+extern char validCommands[];
+bool checksaveDirection(void);
+bool changeDirection(char command);
+void delay_s(void);
+void Delay_s(int x);
+#endif
