7451e0f2ff39466c3899b1e413ba73ed347f031c
ahmedadelhassan159 <104680320+ahmedadelhassan159@users.noreply.github.com>
2024-04-09 06:19:19 +0200

    Update Lcd.c

--- LCD/Lcd.c
+++ LCD/Lcd.c
@@ -1,224 +1,104 @@
-/*
- * Lcd.c
- */
+/******************************************************************************
+ * File:        Lcd.h
+ * Author:      Ahmed Adel Hassan
+ * Created on:  12/2/2024
+ * Description: Header file for controlling an LCD module in an embedded system.
+ ******************************************************************************/
 
-#include "Type.h"
-#include "GPIO.h"
-#include "Lcd.h"
+#ifndef LCD_H_
+#define LCD_H_
 
-static void Cmd(uint08_t data);
-static void Write(uint08_t data);
-static void EnPulse(void);
-static uint08_t* itoa(sint16_t value, uint08_t* buffer, uint16_t base);
-static void Delay(uint32_t t);
+#define LCD_PORT_DATA
+#define LCD_PORT_DIR
+#include "Type.h" // Include necessary header file(s) for types used
 
-
-
-
-/* Description:
- * initialization function
- * set RS , RW , E and data port as output pins
- * */
-void Lcd_Init(void)
-{
-    GPIO_Init(LCD_PORT_ID, GPIO_PIN_0);
-    GPIO_Init(LCD_PORT_ID, GPIO_PIN_1);
-    GPIO_Init(LCD_PORT_ID, GPIO_PIN_2);
-    GPIO_Init(LCD_PORT_ID, GPIO_PIN_3);
-    GPIO_Init(LCD_PORT_ID, GPIO_PIN_4);
-    GPIO_Init(LCD_PORT_ID, GPIO_PIN_5);
-    GPIO_Init(LCD_PORT_ID, GPIO_PIN_6);
-    GPIO_Init(LCD_PORT_ID, GPIO_PIN_7);
-
-    GPIO_SetPortDirection(LCD_PORT_ID, GPIO_DIRECTION_OUTPUT);
-    GPIO_SetPortState(LCD_PORT_ID, 0x00);
-
-    Delay(5000);
-    Write(LCD_TWO_LINES_FOUR_BITS_MODE);
-
-    EnPulse();
-    Delay(200);
-    Write(LCD_CLEAR_COMMAND);
-    Write(LCD_CURSOR_OFF);
-    Write(LCD_ENTRY_MODE);
-
-}
-
-/* Description:
- * this function used to clear the  of LCD
- * */
-void LCD_clearScreen(void){
-    Cmd(LCD_CLEAR_COMMAND);        /* clear the screen*/
-}
+/*LCD command */
+#define LCD_CLEAR_COMMAND              0x01
+#define LCD_GO_TO_HOME                 0x02
+#define LCD_CURSOR_OFF                 0x0C
+#define LCD_CURSOR_ON                  0x0E
+#define LCD_CURSOR_BLINKING            0x0F
+#define LCD_SET_CURSOR_LOCATION        0x80    /*force cursor to beginning of the 1st line */
+#define LCD_TWO_LINES_FOUR_BITS_MODE   0x28
+#define LCD_ENTRY_MODE                 0x06
 
 
-/*Description
- * this function used to display a string on the screen of the LCD
- * */
-void LCD_displayString(char * str)
-{
-    uint08_t i = 0;
-    while(str[i] != '\0')
-    {
-        Data(str[i]);
-        i++;
-    }
-}
+#define LCD_PORT_ID                    GPIO_PORTB
+
+#define LCD_RS_PORT_ID                 GPIO_PORTB
+#define LCD_RS_PIN_ID                  GPIO_PIN_4
+
+#define LCD_E_PORT_ID                  GPIO_PORTB
+#define LCD_E_PIN_ID                   GPIO_PIN_7
+
+#define LCD_RW_PORT_ID                 GPIO_PORTB
+#define LCD_RW_PIN_ID                  GPIO_PIN_5
 
 
-/* Description:
- * this function used to move the cursor to specific row and column
- * */
-void LCD_moveCursorXY(uint08_t row , uint08_t column){
-        uint08_t address;
+/******************************************************************************
+ * Function:    Lcd_VoidInit
+ * Description: Initialization function.
+ *              Sets RS, RW, E, and data port as output pins.
+ * Parameters:  None
+ * Returns:     None
+ ******************************************************************************/
+void Lcd_VoidInit(void);
 
-        switch(row)
-        {
-        case 0 :
-            address= column ;
-            break;
-        case 1 :
-            address= 0x40 + column ;
-            break;
-        case 2 :
-            address= 0x10 + column ;
-            break;
-        case 3 :
-            address= 0x50 + column ;
-            break;
-        }
-        Cmd(LCD_SET_CURSOR_LOCATION | address);
+/******************************************************************************
+ * Function:    LCD_VoidclearScreen
+ * Description: Clears the screen of the LCD.
+ * Parameters:  None
+ * Returns:     None
+ ******************************************************************************/
+void LCD_VoidclearScreen(void);
 
-}
+/******************************************************************************
+ * Function:    LCD_VoidDisplayString
+ * Description: Displays a string on the screen of the LCD.
+ * Parameters:
+ *              str: Pointer to the string to be displayed.
+ * Returns:     None
+ ******************************************************************************/
+void LCD_VoidDisplayString(char * str);
 
-/*
- * Description :
- * Display the string in the required position on the screen
- */
-void LCD_displayStringRowColumn(uint08_t row,uint08_t col,const char *Str){
+/******************************************************************************
+ * Function:    LCD_VoidMoveCursorXY
+ * Description: Moves the cursor to a specific row and column.
+ * Parameters:
+ *              r: Row number (starting from 0).
+ *              col: Column number (starting from 0).
+ * Returns:     None
+ ******************************************************************************/
+void LCD_VoidMoveCursorXY(unsigned char r, unsigned char col);
 
-    LCD_moveCursorXY(row,col);   /* go to to the required LCD position */
-    LCD_displayString(Str);      /* display the string */
-}
+/******************************************************************************
+ * Function:    LCD_VoidDisplayStringRowColumn
+ * Description: Displays the string in the required position on the screen.
+ * Parameters:
+ *              row: Row number (starting from 0).
+ *              col: Column number (starting from 0).
+ *              Str: Pointer to the string to be displayed.
+ * Returns:     None
+ ******************************************************************************/
+void LCD_VoidDisplayStringRowColumn(uint08_t row,uint08_t col,const char *Str);
 
+/******************************************************************************
+ * Function:    LCD_VoidIntgerToString
+ * Description: Displays the required decimal value on the screen.
+ * Parameters:
+ *              data: Integer value to be converted to string and displayed.
+ * Returns:     None
+ ******************************************************************************/
+void LCD_VoidIntgerToString(sint16_t data);
 
-static void Write(uint08_t data)
-{
-    uint08_t d;
-    /* out the last 4 bits of the required command to the data bus D4 --> D7 */
-    d = GPIO_readPort(LCD_PORT_ID);
-    d &= 0x0F;
-    d |= (data & 0xF0);
-    GPIO_SetPortState(LCD_PORT_ID, d);
-    EnPulse();
-    /*repeat for second nibble */
-    d = GPIO_readPort(LCD_PORT_ID);
-    d &= 0x0F;
-    d |= data<<4;
-    GPIO_SetPortState(LCD_PORT_ID, d);
-    EnPulse();
-
-    Delay(2000);
-}
-
-void Data(uint08_t data)
-{
-    GPIO_SetPinState(LCD_RS_PORT_ID, LCD_RS_PIN_ID, GPIO_STATE_HIGH);
-    Write(data);
-}
-
-static void Cmd(uint08_t data)
-{
-    GPIO_SetPinState(LCD_RS_PORT_ID, LCD_RS_PIN_ID, GPIO_STATE_LOW);
-    Write(data);
-}
-
-static void EnPulse(void)
-{
-    GPIO_SetPinState(LCD_E_PORT_ID, LCD_E_PIN_ID, GPIO_STATE_HIGH);
-    Delay(1);
-    GPIO_SetPinState(LCD_E_PORT_ID, LCD_E_PIN_ID, GPIO_STATE_LOW);
-    Delay(1);
-}
+/******************************************************************************
+ * Function:    LCD_VoidData
+ * Description: Sends data to the LCD.
+ * Parameters:
+ *              data: Data to be sent to the LCD.
+ * Returns:     None
+ ******************************************************************************/
+void LCD_VoidData(uint08_t data);
 
 
-/*  implement   itoa()   function
-value – Value to be converted to a string.
-buffer – Array to store the result.
-base – Numerical base used to represent the value as a string, between 2 and 16
-Return Value: a pointer to the resulting null-terminated string, same as parameter buffer.
-*/
-static uint08_t* itoa(sint16_t value, uint08_t* buffer, uint16_t base)
-{
-    // invalid input
-    if (base < 2 || base > 16) {
-        return buffer;
-    }
-    sint16_t n = value;
-    // consider the absolute value of the number
-    if(n<0){
-        n=0-n ;
-    }
-
-    uint16_t i = 0;
-    while (n)
-    {
-        sint16_t r = n % base;
-
-        if (r >= 10) {
-            buffer[i++] = 65 + (r - 10);
-        }
-        else {
-            buffer[i++] = 48 + r;
-        }
-
-        n = n / base;
-    }
-
-    // if the number is 0
-    if (i == 0) {
-        buffer[i++] = '0';
-    }
-
-    /* If the base is 10 and the value is negative, the resulting string
-    is preceded with a minus sign (-)
-    With any other base, value is always considered unsigned */
-
-    if (value < 0 && base == 10) {
-        buffer[i++] = '-';
-    }
-
-    buffer[i] = '\0';                 // null terminate string
-
-    // reverse the string and return it
-    uint16_t k=0, j=i-1 ;
-    while (k < j) {
-        uint08_t *x= &buffer[k++], *y= &buffer[j--], t ;
-        t = *x; *x = *y; *y = t;      // to swap two numbers
-
-    }
-    return buffer;
-}
-
-/*
- * Description :
- * Display the required decimal value on the screen
- */
-void LCD_intgerToString(sint16_t data)
-{
-    uint08_t buffer[16]; /* String to hold the ascii result */
-    itoa(data,buffer,10); /* Use itoa to convert the data to its corresponding ASCII value, 10 for decimal */
-    LCD_displayString(buffer); /* Display the string */
-}
-
-
-
-
-static void Delay(uint32_t t)
-{
-    uint32_t i;
-    for(i=0;i<t*3;i++)
-    {
-        __asm__("NOP");
-    }
-}
+#endif /* LCD_H_ */
