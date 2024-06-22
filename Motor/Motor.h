b04035817c125600c07c086efe7ffc1bac1ea41c
AhmedAbdElmotelbAli <104862736+AhmedAbdElmotelbAli@users.noreply.github.com>
2024-06-22 12:11:18 +0300

    Add files via upload

--- Motor/Motor.h
+++ Motor/Motor.h
@@ -1,75 +1,99 @@
-/****************************************************************************/
-/************************* Name: Ahmed Abdelmotelb Ali     ******************/
-/*************************    Date: 25/2/2024             ******************/
-/*************************      SWC: Motor          	   ******************/
-/*************************    Version: 2.0                 ******************/
-/****************************************************************************/
+/*
+ ============================================================================
+ File Name		: Motor.h
+ Author			: Ahmed Abdelmotelb Ali
+ Version		: 4.0
+ Date			: 5/4/2023
+ Description	: This file contains the implementation of Motor Driver
+ Notes			: 
+ ============================================================================
+ */
 #ifndef _MOTOR_H_
 #define _MOTOR_H_
+#include "stdbool.h"
+#include "GPIO.h"
+#include "Port.h"
+#include <stdint.h>
+#include "PWM.h"
 
 
+#include "config.h"
+extern uint16_t duty_cycle;
+extern uint16_t freq ;
+/*
 typedef enum
 {
-    MOTOR_SPEED_LOW,
+  	
+    MOTOR_SPEED_LOW=1,
+	  MOTOR_SPEED_TWO,
+	  MOTOR_SPEED_THREE,
+	  MOTOR_SPEED_FOUR,
     MOTOR_SPEED_MID,
-    MOTOR_SPEED_HIGH
+	  MOTOR_SPEED_SIX,
+	  MOTOR_SPEED_SEVEN,
+	  MOTOR_SPEED_EIGHT,
+	  MOTOR_SPEED_NI,
+	  MOTOR_SPEED_HIGH
 
-}Motor_Speed_t;
-/* Function Name: Motor_Init
+}Motor_Speed_t;*/
+/* Function Name: Motor_VoidInit
  * Inputs:        Nothing
  * Returns:       void 
  * Pins :        init pin PF0,PF1 TO GENERATE PWM AND PA PIN PA2,PA3,PA6,PA7 IN1,IN2,IN3,IN4
  * Description:   GENERATE PWM AND INIT PORTA
  * */
-void Motor_Init(void);
-/* Function Name: Motor_Start
+void Motor_VoidInit(void);
+/* Function Name: Motor_ViodStart
  * Inputs:        Nothing
  * Returns:       void 
  * Pins :        Enable PF0,PF1
  * Description:   Enable PWM
  * */
-void Motor_Start(void);
-/* Function Name: Motor_Stop
+void Motor_ViodStart(void);
+/* Function Name: Motor_VoidStop
  * Inputs:        Nothing
  * Returns:       void 
  * Pins :        PUT LOW IN IN1,IN2,IN3,IN4
  * Description:  STOP TWO MOTOR
  * */
-void Motor_Stop(void);
-/* Function Name: Motor_GoForward
+void Motor_VoidStop(void);
+/* Function Name: Motor_VoidGoForward
  * Inputs:        Nothing
  * Returns:       void 
  * Pins :       PUT HIGH IN IN1,IN3 low other
  * Description:   MAKE RC GO FORWORD
  * */
-void Motor_GoForward(void);
-/* Function Name: Motor_SetSpeed
+void Motor_VoidGoForward(void);
+/* Function Name: Motor_VoidSetSpeed
  * Inputs:        Motor_Speed_t
  * Returns:       void 
  * Pins :       DO DUTYCYCLE
  * Description:   PUT DUTY CYCLE ON PIN
  * */
-void Motor_SetSpeed(Motor_Speed_t speed);
-/* Function Name: Motor_GoForward
+void Motor_VoidSetSpeed(uint16_t speed);
+/* Function Name: Motor_VoidGoForward
  * Inputs:        Nothing
  * Returns:       void 
  * Pins :       PUT HIGH IN IN2,IN4 low other
  * Description:   MAKE RC GO Backward
  * */
-void Motor_Backward(void);
-/* Function Name: Motor_Right
+void Motor_VoidBackward(void);
+/* Function Name: Motor_VoidRight
  * Inputs:        Nothing
  * Returns:       void 
  * Pins :       PUT HIGH IN IN1 low other
  * Description:   MAKE RC GO Right
  * */
-void Motor_Right(void);
-/* Function Name: Motor_LEFT
+void Motor_VoidRight(void);
+/* Function Name: Motor_VoidLEFT
  * Inputs:        Nothing
  * Returns:       void 
  * Pins :       PUT HIGH IN IN3 low other
  * Description:   MAKE RC GO LEFT
  * */
-void Motor_LEFT(void);
-
+void Motor_VoidLEFT(void);
+void Motor_VoidRight_B(void);
+void Motor_VoidLEFT_B(void);
+void Motor_VoidWait(char data);
+bool Motor_BoolCheckstop(void);
 #endif
--- Motor/Port.h
+++ Motor/Port.h
@@ -1,26 +1,31 @@
-/****************************************************************************/
-/************************* Name: Ahmed Abdelmotelb Ali     ******************/
-/*************************    Date: 25/2/2023             ******************/
-/*************************      SWC: Motor port             ****************/
-/*************************    Version: 2.0                 ******************/
-/****************************************************************************/
+/*
+ ============================================================================
+ File Name		: Port.h
+ Author			: Ahmed Abdelmotelb Ali
+ Version		: 4.0
+ Date			: 5/4/2023
+ Description	: This file contains the implementation of Port Driver
+ Notes			: 
+ ============================================================================
+ */
 #ifndef _PORT_H_
 #define _PORT_H_
 #include "config.h"
 
 /* ***************** This is a Temp Solution ***************** */
-#define MOTOR_1_PORT       (GPIO_PORTA)
-#define MOTOR_2_PORT       (GPIO_PORTA)
-/***************Motor 1***************/
 
-#define MOTOR_1_ENABLE     (GPIO_PIN_0)           /*PF0 PWM */
-#define MOTOR_1_IN1        (GPIO_PIN_2)
-#define MOTOR_1_IN2        (GPIO_PIN_3)
+#define MOTOR_3_PORT       (GPIO_PORTA)
+#define MOTOR_4_PORT       (GPIO_PORTC)
 
-/***************Motor 2***************/
-#define MOTOR_2_ENABLE     (GPIO_PIN_1)           /*PF1 PWM */
-#define MOTOR_2_IN1        (GPIO_PIN_6)
-#define MOTOR_2_IN2        (GPIO_PIN_7)
+/***************Motor 3***************/
+#define MOTOR_3_ENABLE     (GPIO_PIN_2)			  /*PF0 PWM */
+#define MOTOR_3_IN1        (GPIO_PIN_3)
+#define MOTOR_3_IN2        (GPIO_PIN_2)
+
+/***************Motor 4***************/
+#define MOTOR_4_ENABLE     (GPIO_PIN_3)			   /*PF2 PWM */
+#define MOTOR_4_IN1        (GPIO_PIN_6)
+#define MOTOR_4_IN2        (GPIO_PIN_7)
 
 
 #endif
